c8a86e5e12d34985c8004d89d25a02f24c1b384d
lmabaso <liberty.mabaso@yahoo.com>
2018-06-07 14:22:35 +0200

    lmabaso

--- Libft/main.c
+++ Libft/main.c
@@ -55,11 +55,28 @@ void testMemcpy(void)
 		printf("[Failed]\n");
 }
 
+void testMemccpy(void)
+{
+	char str[50];
+	char tstr[50];
+	char var[] = "liberty";
+
+	memccpy(str, var, 't', 7);
+	ft_memccpy(tstr, var,'t', 7);
+
+	printf("Memccpy\t");
+	if (memcmp(str, tstr, 7) == 0)
+		printf("[Pass]\n");
+	else
+		printf("[Failed]\n");
+}
+
 int		main(void)
 {
 	
 	testMemset();
 	testBzero();
 	testMemcpy();
+	testMemccpy();
 	return(0);
 }
