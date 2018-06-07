61083685bbcf861e85f0653f5e5e37e742400de2
lmabaso <liberty.mabaso@yahoo.com>
2018-06-07 13:43:21 +0200

    lmabaso

--- Libft/main.c
+++ Libft/main.c
@@ -13,16 +13,36 @@ void testMemset(void)
 
 	ft_memset(str, '#', 4);
 	memset(tstr, '#', 4);
-
+	printf("Memset\t");
 	if (memcmp(str, tstr, 7) == 0)
-		printf("Pass\n");
+		printf("[Pass]\n");
 	else
-		printf("Failed\n");
+		printf("[Failed]\n");
 
+}
+
+void testBzero(void)
+{
+	char str[50];
+	char tstr[50];
+	char var[] = "liberty";
+
+	strcpy(str, var);
+	strcpy(tstr, var);
+
+	ft_bzero(str, 4);
+	bzero(tstr, 4);
+	printf("Bzero\t");
+	if (memcmp(str, tstr, 7) == 0)
+		printf("[Pass]\n");
+	else
+		printf("[Failed]\n");
 }
 
 int		main(void)
 {
-	testMemset();
+	
+	//testMemset();
+	testBzero();
 	return(0);
 }
