6b4bc84342e24910eed531a0b5a80dfe829aac1b
mnyembe <mongi.nyembe@gmail.com>
2018-06-13 14:43:03 +0200

    13 june 14:42

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -1,11 +1,21 @@
 #include "libft.h"
+#include "ctype.h"
 
 int	ft_atoi(const char *str)
 {
-	int n;
+	int sign;
+	long num;
 
-	n = 1;
-	while (isspace(*str))
+	sign = 1;
+	num = 0;
+	while (ft_isspace(*str) || *str == '+')
 		str++;
-
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (isdigit(*str))
+		num = num * 10 + (*str++ - '0');
+	return ((int)num * sign);
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -24,5 +24,11 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int	ft_strcmp(const char *s1, const char *s2);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_atoi(const char *str);
+int	ft_isspace(int c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
 #endif
