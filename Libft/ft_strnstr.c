1e20f79cb659607f588a579cd980a95edcbc585a
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2018-06-16 17:55:41 +0200

    liberty

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -7,14 +7,19 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		return ((char *)haystack);
 	if (ft_strlen(haystack) < ft_strlen(needle))
 		return (NULL);
-	while (len--)
+	while (len)
 	{
 		if (!needle)
 			break ;
 		if (*haystack == *needle)
+		{
+			if (ft_strlen(haystack) < ft_strlen(needle))
+				break ;
 			if (ft_strncmp(haystack, needle, ft_strlen(needle)) == 0)
 				return ((char *)haystack);
+		}
 		haystack++;
+		len--;
 	}
 	return (NULL);
 }
